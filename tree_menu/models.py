49c5ae1a81355f55bc0f2589a91ab014bab6ed97
Gama_Ray <gamaizingg@gmail.com>
2024-02-06 12:16:52 +0300

    Добавлена модель Menu, а также файл constants

--- tree_menu/models.py
+++ tree_menu/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from constants import NULLABLE
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=255, verbose_name='меню')
+    branch = models.ForeignKey('self', **NULLABLE, verbose_name='ветка')
+
+    def __str__(self):
+        return self.name
